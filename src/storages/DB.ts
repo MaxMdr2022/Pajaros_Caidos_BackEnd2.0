51821663561377af9dfbaef646e5681fbce659de
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-07-25 20:02:53 -0300

    added category, item and news models

--- src/storages/DB.ts
+++ src/storages/DB.ts
@@ -5,6 +5,9 @@ import CommentModel from '../models/models/Comment'
 import BirdModel from '../models/models/Bird'
 import PublicationModel from '../models/models/Publication'
 import ReactionModel from '../models/models/Reaction'
+import CategoryModel from '../models/models/Category'
+import ItemModel from '../models/models/Item'
+import NewsModel from '../models/models/News'
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env
 
@@ -21,6 +24,9 @@ const CommentsListModel = CommentModel(database)
 const BirdsListModel = BirdModel(database)
 const PublicationsListModel = PublicationModel(database)
 const ReactionsListModel = ReactionModel(database)
+const ItemListModel = ItemModel(database)
+const CategoryListModel = CategoryModel(database)
+const NewsListModel = NewsModel(database)
 
 UserListModel.hasMany(CommentsListModel)
 CommentsListModel.belongsTo(UserListModel)
@@ -37,11 +43,17 @@ CommentsListModel.belongsTo(PublicationsListModel)
 PublicationsListModel.hasMany(ReactionsListModel)
 ReactionsListModel.belongsTo(PublicationsListModel)
 
+ItemListModel.belongsToMany(CategoryListModel, { through: 'item_category' })
+CategoryListModel.belongsToMany(ItemListModel, { through: 'item_category' })
+
 export {
   UserListModel,
   CommentsListModel,
   PublicationsListModel,
   ReactionsListModel,
   BirdsListModel,
+  ItemListModel,
+  CategoryListModel,
+  NewsListModel,
   database,
 }
