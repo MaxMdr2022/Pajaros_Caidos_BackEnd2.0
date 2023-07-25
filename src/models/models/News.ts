00c1e981b63be8ce9d465339ee89598de61ded8c
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-07-25 20:07:07 -0300

    news model

---
+++ src/models/models/News.ts
@@ -0,0 +1,41 @@
+import { DataTypes } from 'sequelize'
+
+export default (sequelize) => {
+  const News = sequelize.define(
+    'news',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
+      // video: {
+      //     type: DataTypes.ARRAY(DataTypes.STRING),
+      //     allowNull: false
+      // },
+      isDeleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  )
+
+  return News
+}
