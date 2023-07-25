48a8504e27ffaf9bf3e424500a3ce069ec5bcb71
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-07-25 20:11:49 -0300

    changes in comments facade

--- src/facades/CommentFacade.ts
+++ src/facades/CommentFacade.ts
@@ -1,22 +1,39 @@
 import { PostgresDBStorage } from '../storages/PostgresDBStorage'
 import { Comment } from '../models/types/Comment'
+import { CommentsListModel } from '../storages/DB'
 
 const storage = new PostgresDBStorage()
 
 export class CommentFacade {
   async getCommentById(id: string): Promise<Comment> {
-    return await storage.findCommentById(id)
+    return await storage.findById(CommentsListModel, id)
   }
 
-  async createComment(id: string, data: any): Promise<Comment> {
-    return await storage.createComment(id, data)
+  async createComment(publication: any, data: any): Promise<Comment> {
+    const { comment, user } = data
+
+    const newComment: Comment = await storage.create(CommentsListModel, { comment })
+
+    await storage.relationship(user, 'addComment', newComment)
+
+    await storage.relationship(publication, 'addComment', newComment)
+
+    return newComment
   }
 
   async updateComment(id: string, comment: string): Promise<Comment> {
-    return await storage.updateComment(id, comment)
+    return await storage.update(CommentsListModel, { comment }, { where: { id } })
   }
 
-  async deleteComment(id: string): Promise<Comment> {
-    return await storage.deleteComment(id)
+  async deleteComment(comment: any): Promise<Comment> {
+    const filter: any = {
+      where: {
+        id: comment.id,
+        userId: comment.userId,
+        publicationId: comment.publicationId,
+      },
+    }
+
+    return await storage.delete(CommentsListModel, filter)
   }
 }
