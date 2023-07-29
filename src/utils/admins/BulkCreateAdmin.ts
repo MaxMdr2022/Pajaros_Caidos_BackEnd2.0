96ea4fbbe511776c228fb0590a8c24d656ea631c
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-07-28 23:18:31 -0300

    fix error in the bulkCreateAdmins

--- src/utils/admins/BulkCreateAdmin.ts
+++ src/utils/admins/BulkCreateAdmin.ts
@@ -1,19 +1,9 @@
 import { UserListModel } from '../../storages/DB'
-import fs from 'fs'
-import path from 'path'
 import bcrypt from 'bcrypt'
+import UsersAdmins from '../admins/UsersAdmins'
 
 const bulkCreateAdmin = async () => {
-  const __dirname = path.resolve()
-  const data = fs.readFileSync(
-    path.join(__dirname, '../Back-End/src/utils/admins/UsersAdmins.json'),
-    'utf8'
-  )
-  const objectJSON = JSON.parse(data)
-
-  //   console.log('dataJSON ', objectJSON[0].first_name)
-
-  for (let i = 0; i < objectJSON.length; i++) {
+  for (let i = 0; i < UsersAdmins.length; i++) {
     const pass = process.env[`ADMIN_${i + 1}_PASS`]
 
     const email = process.env[`ADMIN_${i + 1}_EMAIL`]
@@ -26,13 +16,13 @@ const bulkCreateAdmin = async () => {
       defaults: {
         email: email,
         password: hashedPassword,
-        first_name: objectJSON[i].first_name,
-        last_name: objectJSON[i].last_name,
-        nick_name: objectJSON[i].nick_name,
-        avatar: objectJSON[i].avatar,
-        isAdmin: objectJSON[i].isAdmin,
-        isPrincipalAdmin: objectJSON[i].isPrincipalAdmin,
-        userEmailValidate: objectJSON[i].userEmailValidate,
+        first_name: UsersAdmins[i].first_name,
+        last_name: UsersAdmins[i].last_name,
+        nick_name: UsersAdmins[i].nick_name,
+        avatar: UsersAdmins[i].avatar,
+        isAdmin: UsersAdmins[i].isAdmin,
+        isPrincipalAdmin: UsersAdmins[i].isPrincipalAdmin,
+        userEmailValidate: UsersAdmins[i].userEmailValidate,
       },
     })
   }
---
+++ src/utils/admins/UsersAdmins.ts
@@ -0,0 +1,24 @@
+const UsersAdmins = [
+  {
+    first_name: 'Tony',
+    last_name: 'Stark',
+    nick_name: 'Administrador',
+    avatar:
+      'https://t4.ftcdn.net/jpg/04/75/00/99/360_F_475009987_zwsk4c77x3cTpcI3W1C1LU4pOSyPKaqi.jpg',
+    isAdmin: true,
+    isPrincipalAdmin: true,
+    userEmailValidate: true,
+  },
+  {
+    first_name: 'Clara',
+    last_name: 'Correa',
+    nick_name: 'Administrador',
+    avatar:
+      'https://t4.ftcdn.net/jpg/04/75/00/99/360_F_475009987_zwsk4c77x3cTpcI3W1C1LU4pOSyPKaqi.jpg',
+    isAdmin: true,
+    isPrincipalAdmin: true,
+    userEmailValidate: true,
+  },
+]
+
+export default UsersAdmins
