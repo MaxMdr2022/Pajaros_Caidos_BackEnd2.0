8bb5af0abdd24ec2ba1edb5b6b8f7f2997f73180
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-08-06 19:46:43 -0300

    server

--- src/StartServer.ts
+++ src/StartServer.ts
@@ -34,7 +34,7 @@ class StartServer extends Server {
     this.app.use(parserJson())
     this.app.use(express.json())
     this.app.use(express.urlencoded({ extended: true }))
-    this.app.use(cors({ credentials: true }))
+    this.app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 
     super.addControllers([
       new UserController(),
