75512bf4f0a3aa40a9e26aaa4904e805fa6efe86
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-08-06 20:54:02 -0300

    changes in function

--- src/controllers/middlewares/Authentications.ts
+++ src/controllers/middlewares/Authentications.ts
@@ -3,13 +3,11 @@ import { ErrorCodeType, ErrorResponse } from '../../models/responses/ErrorRespon
 import jwt from 'jsonwebtoken'
 
 export async function validateToken(req: Request, res: Response, next: NextFunction) {
-  const headerToken = req.headers['authorization'] // =>  "Bearer asd12h21g3fgh123ssda22sd32f23"
+  const { JWT } = req.cookies()
 
-  if (headerToken !== undefined && headerToken.startsWith('Bearer')) {
+  if (JWT) {
     try {
-      const tokenSinBearer = headerToken.slice(7)
-
-      jwt.verify(tokenSinBearer, process.env.SECRET_KEY_JWT)
+      jwt.verify(JWT, process.env.SECRET_KEY_JWT)
 
       next()
     } catch (error) {
@@ -20,4 +18,22 @@ export async function validateToken(req: Request, res: Response, next: NextFunct
     const message = 'ACCESS DENIED'
     return res.status(400).send(new ErrorResponse(message, ErrorCodeType.AccessDenied))
   }
+
+  // const headerToken = req.headers['authorization'] // =>  "Bearer asd12h21g3fgh123ssda22sd32f23"
+
+  // if (headerToken !== undefined && headerToken.startsWith('Bearer')) {
+  //   try {
+  //     const tokenSinBearer = headerToken.slice(7)
+
+  //     jwt.verify(tokenSinBearer, process.env.SECRET_KEY_JWT)
+
+  //     next()
+  //   } catch (error) {
+  //     const message = 'INVALID TOKEN'
+  //     return res.status(401).send(new ErrorResponse(message, ErrorCodeType.InvalidToken))
+  //   }
+  // } else {
+  //   const message = 'ACCESS DENIED'
+  //   return res.status(400).send(new ErrorResponse(message, ErrorCodeType.AccessDenied))
+  // }
 }
