265d2ae2a5c51ee1b468f4060b9c457c7737d93f
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-08-06 23:05:21 -0300

    change

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -90,9 +90,7 @@ export class UserController {
 
     const JWT = await helper.getJWTUserLogIn(email)
 
-    res.clearCookie('JWT')
-
-    res.cookie('JWT', JWT, { sameSite: 'lax', secure: true })
+    res.cookie('JWT', JWT, { sameSite: 'none', secure: true })
 
     res.status(200).send(new ResponseSuccess({ user }))
   }
