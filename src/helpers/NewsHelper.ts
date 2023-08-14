13282fdf74fc516fe50b7ffd09b913f9256266df
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-08-14 19:03:26 -0300

    changes in helper

--- src/helpers/NewsHelper.ts
+++ src/helpers/NewsHelper.ts
@@ -1,5 +1,6 @@
 import { NewsFacade } from '../facades/NewsFacade'
-import { News } from '../models/types/News'
+import { News, Response } from '../models/types/News'
+import { Banner } from '../models/types/Banner'
 
 const facade = new NewsFacade()
 
@@ -8,8 +9,23 @@ export class NewsHelper {
     return await facade.getNewsById(id)
   }
 
-  async getAllNews(data?: any): Promise<News> {
-    return await facade.getAllNews(data)
+  async getAllNews(data?: any): Promise<Response> {
+    const { newsPerPage } = data
+
+    const news: News[] = await facade.getAllNews(data)
+
+    if (!news || !news[0]) return { news: [] }
+
+    if (newsPerPage) {
+      const bannerImages: Banner[] = await facade.getAllBannerImages()
+
+      const quantity = await facade.countNews()
+
+      const totalPages = Math.ceil(quantity / newsPerPage)
+
+      return { totalPages, news, banner: bannerImages }
+    }
+    return { news }
   }
 
   async createNews(data: News): Promise<News> {
@@ -22,5 +38,29 @@ export class NewsHelper {
 
   async deleteNews(id: string): Promise<News> {
     return await facade.deleteNews(id)
+  }
+
+  //---------- BANNER -----------------------
+
+  async getBannerImageById(id: string): Promise<Banner> {
+    return await facade.getBannerImageById(id)
+  }
+
+  async getAllBannerImages(): Promise<Banner[]> {
+    const bannerImages: Banner[] = await facade.getAllBannerImages()
+    if (!bannerImages || !bannerImages[0]) return []
+    return bannerImages
+  }
+
+  async createBannerImage(data: Banner): Promise<Banner> {
+    return await facade.addBannerImage(data)
+  }
+
+  async updateBannerImage(id: string, data: any): Promise<Banner> {
+    return await facade.updateBannerImage(id, data)
+  }
+
+  async deleteBannerImage(id: string): Promise<Banner> {
+    return await facade.deleteBannerImage(id)
   }
 }
