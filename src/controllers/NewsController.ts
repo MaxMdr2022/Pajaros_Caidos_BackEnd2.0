c460ae0b2c3456769efeeaa79f0812ed16ce7a06
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-08-14 19:03:41 -0300

    cambios en controlador

--- src/controllers/NewsController.ts
+++ src/controllers/NewsController.ts
@@ -3,9 +3,11 @@ import { Request, Response } from 'express'
 import { NewsHelper } from '../helpers/NewsHelper'
 import { ResponseSuccess } from '../models/responses/ResponseSuccess'
 import {
+  validateDataCreateBanner,
   validateDataCreateNews,
   validateDataUpdate,
   validateId,
+  validateIdBannerImage,
   validateQuery,
 } from './middlewares/NewsMiddleware'
 
@@ -20,7 +22,25 @@ export class NewsController {
 
     const news = await helper.getAllNews(filters)
 
-    res.status(200).send(new ResponseSuccess({ news }))
+    res.status(200).send(new ResponseSuccess(news))
+  }
+
+  //----------- Banner ----------------------
+
+  @Get('banner')
+  @Middleware([])
+  async getBannerImages(req: Request, res: Response) {
+    const images = await helper.getAllBannerImages()
+
+    res.status(200).send(new ResponseSuccess({ images }))
+  }
+
+  @Get('banner/:id')
+  @Middleware([validateIdBannerImage])
+  async getBannerImageById(req: Request, res: Response) {
+    const { image } = res.locals
+
+    res.status(200).send(new ResponseSuccess({ image }))
   }
 
   @Get(':id')
@@ -59,5 +79,37 @@ export class NewsController {
     const newsDeleted = await helper.deleteNews(id)
 
     res.status(200).send(new ResponseSuccess({ newsDeleted }))
+  }
+
+  //----------- Banner ----------------------
+
+  @Post('banner')
+  @Middleware([validateDataCreateBanner])
+  async createBannerImage(req: Request, res: Response) {
+    const { data } = res.locals
+
+    const newImage = await helper.createBannerImage(data)
+
+    res.status(200).send(new ResponseSuccess({ newImage }))
+  }
+
+  @Put('banner/:id')
+  @Middleware([validateIdBannerImage, validateDataCreateBanner])
+  async updateBannerImage(req: Request, res: Response) {
+    const { id, data } = res.locals
+
+    const imageUpdated = await helper.updateBannerImage(id, data)
+
+    res.status(200).send(new ResponseSuccess({ imageUpdated }))
+  }
+
+  @Delete('banner/:id')
+  @Middleware([validateIdBannerImage])
+  async deleteBannerImage(req: Request, res: Response) {
+    const { id } = res.locals
+
+    const imageDeleted = await helper.deleteBannerImage(id)
+
+    res.status(200).send(new ResponseSuccess({ imageDeleted }))
   }
 }
