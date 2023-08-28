b397a8e73f65aadcf3bb0c9cc267f681591a1fd8
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-08-27 21:53:13 -0300

    changes in model publication

--- src/models/types/Publication.ts
+++ src/models/types/Publication.ts
@@ -2,8 +2,12 @@ export type Publication = {
   id?: string
   title: string
   description: string
-  image: string[]
+  image: Image[]
   isDeleted: boolean
   createdAt?: Date
   updatedAt?: Date
+}
+type Image = {
+  public_id: string
+  secure_url: string
 }
