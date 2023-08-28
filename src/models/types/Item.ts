698630fb2644c85600626ea671bed2f2ed812352
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-08-27 21:54:08 -0300

    changes in image prop

--- src/models/types/Item.ts
+++ src/models/types/Item.ts
@@ -2,8 +2,12 @@ export type Item = {
   id?: string
   title: string
   description: string
-  image: string[]
+  image: Image[]
   price: number
   createdAt?: Date
   updatedAt?: Date
+}
+type Image = {
+  public_id: string
+  secure_url: string
 }
