3fadcb545bb23ab66db29033e5c69bf029312ae5
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-08-27 21:59:10 -0300

    helper2

--- src/helpers/AdvertisingHelper.ts
+++ src/helpers/AdvertisingHelper.ts
@@ -1,5 +1,6 @@
 import { AdvertisingFacade } from '../facades/AdvertisingFacade'
 import { Advertising } from '../models/types/Advertising'
+import { deleteImage } from '../utils/cloudinary/Cloudinary'
 
 const facade = new AdvertisingFacade()
 
@@ -16,11 +17,33 @@ export class AdvertisingHelper {
     return await facade.createAdvertising(data)
   }
 
-  async updateAdvertising(id: string, data: any): Promise<Advertising> {
-    return await facade.updateAdvertising(id, data)
+  async updateAdvertising(id: string, advertising: Advertising, data: any): Promise<Advertising> {
+    const { newImages, deleteImages, ...dataUpdated } = data
+
+    const image = advertising.image
+    dataUpdated.image = image
+
+    if (deleteImages && deleteImages[0]) {
+      const imageNoDeleted = image.filter((e) => !deleteImages.find((el) => e.public_id === el))
+
+      dataUpdated.image = imageNoDeleted
+
+      for (const img of deleteImages) {
+        await deleteImage(img)
+      }
+    }
+
+    if (newImages && newImages[0]) {
+      const updatedImages = [...dataUpdated.image, ...newImages]
+      dataUpdated.image = updatedImages
+    }
+    return await facade.updateAdvertising(id, dataUpdated)
   }
 
-  async deleteAdvertising(id: string): Promise<Advertising> {
+  async deleteAdvertising(id: string, advertising: Advertising): Promise<Advertising> {
+    for (const img of advertising.image) {
+      await deleteImage(img.public_id)
+    }
     return await facade.deleteAdvertising(id)
   }
 }
