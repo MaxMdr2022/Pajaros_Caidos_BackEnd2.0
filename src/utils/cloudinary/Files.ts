6ce7595a34f6aabe52b21881bc4e4833c7ce6ba0
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-08-27 21:50:28 -0300

    added files enum

---
+++ src/utils/cloudinary/Files.ts
@@ -0,0 +1,9 @@
+export enum File {
+  NEWS = 'NEWS',
+  PUBLICATIONS = 'PUBLICATIONS',
+  SHOP = 'SHOP',
+  USER = 'USER',
+  ADVERTISING = 'ADVERTISING',
+  BIRDS = 'BIRDS',
+  BANNER = 'BANNER',
+}
