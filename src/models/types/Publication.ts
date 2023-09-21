0fcceac0e66f642b4b88d4599ae8408def4405c3
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-09-21 15:25:17 -0300

    extend type

--- src/models/types/Publication.ts
+++ src/models/types/Publication.ts
@@ -1,3 +1,5 @@
+import { User } from './User'
+
 export type Publication = {
   id?: string
   title: string
@@ -10,4 +12,12 @@ export type Publication = {
 type Image = {
   public_id: string
   secure_url: string
+  imageUrl: string
+}
+
+export type PublicationAndUser = Pick<
+  Publication,
+  'id' | 'title' | 'description' | 'image' | 'isDeleted' | 'createdAt' | 'updatedAt'
+> & {
+  user?: User
 }
