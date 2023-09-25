6a4f5c350c1ca2821bd2cb64ecca9574b14e92ee
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-09-25 18:50:09 -0300

    validate token off

--- src/controllers/PublicationController.ts
+++ src/controllers/PublicationController.ts
@@ -17,7 +17,7 @@ const helper = new PublicationHelper()
 @Controller('publication')
 export class PublicationController {
   @Get('all')
-  @Middleware([validateToken, validateLimitQuery]) //
+  @Middleware([validateLimitQuery]) //validateToken,
   async getAllPublications(req: Request, res: Response) {
     const { data } = res.locals
     const publications = await helper.getAllPublications(data)
