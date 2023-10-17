4b063175601cc166449050bb9b700091d74b4c6c
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-10-17 12:37:31 -0300

    file upload

--- src/controllers/middlewares/FileUploadMiddleware.ts
+++ src/controllers/middlewares/FileUploadMiddleware.ts
@@ -1,10 +1,16 @@
 import { Request, Response, NextFunction } from 'express'
-
+import fs from 'fs'
 import fileUpload from 'express-fileupload'
 
 export async function fileUploadMiddleware(req: Request, res: Response, next: NextFunction) {
+  const tempDir = '/tmp'
+
+  if (!fs.existsSync(tempDir)) {
+    fs.mkdirSync(tempDir)
+  }
+
   fileUpload({
     useTempFiles: true,
-    tempFileDir: './cloudinaryUploads',
+    tempFileDir: tempDir,
   })(req, res, next)
 }
