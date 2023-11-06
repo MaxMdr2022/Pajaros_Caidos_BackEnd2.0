32179176e472c2d83396a488099174e6ea66297f
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-11-06 19:11:21 -0300

    Changes html and function

--- src/utils/nodeMailer/Functions.ts
+++ src/utils/nodeMailer/Functions.ts
@@ -1,52 +1,55 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer'
 
 export const codeUserVerification = () => {
-  const characters = "0123456789";
-  let code = "";
+  const characters = '0123456789'
+  let code = ''
   for (let i = 0; i < 6; i++) {
-    code += characters.charAt(Math.floor(Math.random() * characters.length));
+    code += characters.charAt(Math.floor(Math.random() * characters.length))
   }
-  return code;
-};
+  return code
+}
 
 export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com", //smtp.ethereal.email
+  host: 'smtp.gmail.com', //smtp.ethereal.email
   port: 465, //587
   secure: true, // true for 465, false for other ports
   auth: {
     user: `${process.env.NODEMAILER_GMAIL}`,
     pass: `${process.env.NODEMAILER_PASS_GMAIL}`,
   },
-});
+})
 
 export const mailOption = (email: string, first_name: string, code: string) => {
   const image =
-    "https://res.cloudinary.com/dzu7tm74o/image/upload/v1694363148/STATIC%20IMAGE/obnh0c4aqj2z11pbsqsv_e2h4q6.png";
+    'https://res.cloudinary.com/dzu7tm74o/image/upload/v1699296763/STATIC%20IMAGE/navbar-logo_ljrvgg.webp'
   return {
-    from: "ONG Pajaros Caidos", // sender address
+    from: 'ONG Pajaros Caidos', // sender address
     to: `${email}`, // list of receivers
-    subject: "Active su cuenta Pájaros Caídos ahora", // Subject line
+    subject: 'Active su cuenta Pájaros Caídos ahora', // Subject line
     html: `
-        <div>
-          <p>
-            Hola, ${first_name}
-            Gracias por registrarte en Pájaros Caídos.
-            Copie el siguiente código para verificar su correo electrónico. :D
-          </p>
+      <div style="text-align: center; background-color: #790595e0; padding: 20px;">
+        <p>
+          Hola, ${first_name} Gracias por registrarte en Pájaros Caídos. Copie el siguiente código para verificar su correo electrónico. :D
+        </p>
+        <p style="background-color: #ffffff; color: #000000;">
           ${code}
-          <img src=${image} alt="imagen logo PC">
-          <p>ONG Pajaros Caidos</p>
-        </div>`,
+        </p>
+        <div style="border:solid white; display: inline-block; border-radius: 50%; background-color:#000000bf; width: 300px; height: 300px;">
+          <img src='${image}' alt="imagen-logo-PajarosCaidos" style=" width: 300px; height: 250px; ">
+        </div>
+        <p>ONG Pajaros Caidos</p>
+    </div>
+    `,
 
     // text: ``
-  };
-};
+  }
+}
 
 export const mailOptionGeneratePassword = (email: string, first_name: string, newPass: string) => {
   return {
-    from: "ONG Pajaros Caidos", // sender address
+    from: 'ONG Pajaros Caidos', // sender address
     to: `${email}`, // list of receivers
-    subject: "Recuperación de la cuenta Pajaros Caidos", // Subject line
+    subject: 'Recuperación de la cuenta Pajaros Caidos', // Subject line
     html: `
         <div>
           <p>
@@ -60,15 +63,19 @@ export const mailOptionGeneratePassword = (email: string, first_name: string, ne
         </div>`,
 
     // text: ``
-  };
-};
+  }
+}
 
-export const sendEmail = (mailOption: any) => {
-  transporter.sendMail(mailOption, async (err, info) => {
-    if (err) {
-      return "Failed to send verification email.";
-    } else {
-      return "Verification email sent successfully.";
-    }
-  });
-};
+export const sendEmail = (mailOption: any): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOption, (err, info) => {
+      if (err) {
+        console.log('Error al enviar el email')
+        reject('Failed to send verification email.')
+      } else {
+        console.log('El email se envió con éxito.')
+        resolve('Verification email sent successfully.')
+      }
+    })
+  })
+}
