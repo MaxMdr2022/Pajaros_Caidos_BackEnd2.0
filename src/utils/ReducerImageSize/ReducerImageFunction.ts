98fcf9d9edc0e6fa43390501583205ce8773b7ce
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-11-30 19:22:11 -0300

    logss

--- src/utils/ReducerImageSize/ReducerImageFunction.ts
+++ src/utils/ReducerImageSize/ReducerImageFunction.ts
@@ -1,43 +1,46 @@
 import sharp from 'sharp'
 import { UploadedFile } from 'express-fileupload'
 
-export const reducerImageSize = async (imagePaths: UploadedFile | UploadedFile[] | undefined) => {
+export const reducerImageSize = async (image: UploadedFile | UploadedFile[] | undefined) => {
   const MAX_IMAGE_SIZE_BYTES = 550 * 1024 // 550kb
 
   try {
-    if (!Array.isArray(imagePaths)) {
-      const imageInfo = await sharp(imagePaths.tempFilePath).metadata()
+    if (!Array.isArray(image)) {
+      const imageInfo = await sharp(image.tempFilePath).metadata()
+      console.log('image: ', image.tempFilePath)
+      console.log('image info; ', imageInfo)
+
       console.log('not array size: ', imageInfo.size)
 
       if (imageInfo.size > MAX_IMAGE_SIZE_BYTES) {
         console.log('entro not a')
 
-        const outputImageBuffer: Buffer | UploadedFile = await sharp(imagePaths.tempFilePath)
+        const outputImageBuffer: Buffer | UploadedFile = await sharp(image.tempFilePath)
           .resize({ width: 800 })
           .jpeg({ quality: 60 })
           .toBuffer()
 
         return outputImageBuffer
       } else {
-        return imagePaths
+        return image
       }
     } else {
       const compressedImages: (Buffer | UploadedFile)[] = await Promise.all(
-        imagePaths.map(async (imagePath) => {
-          const imageInfo = await sharp(imagePath.tempFilePath).metadata()
+        image.map(async (img) => {
+          const imageInfo = await sharp(img.tempFilePath).metadata()
           console.log('array size: ', imageInfo.size)
 
           if (imageInfo.size > MAX_IMAGE_SIZE_BYTES) {
             console.log('entro a')
 
-            const outputImageBuffer = await sharp(imagePath.tempFilePath)
+            const outputImageBuffer = await sharp(img.tempFilePath)
               .resize({ width: 800 })
               .jpeg({ quality: 60 })
               .toBuffer()
 
             return outputImageBuffer
           } else {
-            return imagePath
+            return img
           }
         })
       )
