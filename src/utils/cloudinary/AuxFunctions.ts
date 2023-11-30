c4dfeaa468f051a6ee38d8b2c4f9f1dbe351f95e
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-11-30 19:51:16 -0300

    buffer

--- src/utils/cloudinary/AuxFunctions.ts
+++ src/utils/cloudinary/AuxFunctions.ts
@@ -2,12 +2,12 @@ import { uploadImage } from './Cloudinary'
 import fs from 'fs-extra'
 import { UploadedFile } from 'express-fileupload'
 
-export const uploadImg = async (image: UploadedFile | UploadedFile[], file: string) => {
+export const uploadImg = async (image: Buffer | Buffer[], file: string) => {
   const data = []
 
   if (!Array.isArray(image)) {
     // Si image no es un array, asumimos que es un solo UploadedFile
-    const response = await uploadImage(image.tempFilePath, file)
+    const response = await uploadImage(image, file)
 
     if (!response) {
       const message = 'Error Cloudinary response'
@@ -16,18 +16,18 @@ export const uploadImg = async (image: UploadedFile | UploadedFile[], file: stri
 
     data.push({ public_id: response.public_id, secure_url: response.secure_url })
 
-    await fs.unlink(image.tempFilePath) // eliminamos la imagen que se guardo en el servidor
+    await fs.unlink(image) // eliminamos la imagen que se guardo en el servidor
   } else {
     // Si image es un array, asumimos que es un array de UploadedFile
     for (const img of image) {
-      const response = await uploadImage(img.tempFilePath, file)
+      const response = await uploadImage(img, file)
       if (!response) {
         const message = 'Error Cloudinary response'
         return message
       }
 
       data.push({ public_id: response.public_id, secure_url: response.secure_url })
-      await fs.unlink(img.tempFilePath)
+      await fs.unlink(img)
     }
   }
 
